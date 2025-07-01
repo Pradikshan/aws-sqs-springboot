95651553fd37b75d72e62155fb029f876f671432
Pradikshan <Pradikshan>
2025-07-01 21:07:37 +0530

    Added model

---
+++ src/main/java/com/example/aws_sqs/model/Order.java
@@ -0,0 +1,27 @@
+package com.example.aws_sqs.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "orders")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "status", nullable = false)
+    private boolean status;
+
+    @Column(name = "created_date", nullable = false)
+    private LocalDateTime created_date;
+}
